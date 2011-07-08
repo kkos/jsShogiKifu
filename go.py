d6dd2efe14cd080ba4f22739aa4d199493329a08
K.Kosako <sndgk393@ybb.ne.jp>
2011-07-08 22:07:23 +0900

    add server script for kif file Pragma: no-cache

---
+++ go.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import os
+import posixpath
+import SimpleHTTPServer
+
+class HookHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        """Serve a GET request."""
+        f = self.send_head()
+        if f:
+            self.copyfile(f, self.wfile)
+            f.close()
+
+    def send_head(self):
+        path = self.translate_path(self.path)
+        f = None
+        if os.path.isdir(path):
+            if not self.path.endswith('/'):
+                # redirect browser - doing basically what apache does
+                self.send_response(301)
+                self.send_header("Location", self.path + "/")
+                self.end_headers()
+                return None
+            for index in "index.html", "index.htm":
+                index = os.path.join(path, index)
+                if os.path.exists(index):
+                    path = index
+                    break
+            else:
+                return self.list_directory(path)
+        ctype = self.guess_type(path)
+        try:
+            # Always read in binary mode. Opening files in text mode may cause
+            # newline translations, making the actual size of the content
+            # transmitted *less* than the content-length!
+            f = open(path, 'rb')
+        except IOError:
+            self.send_error(404, "File not found")
+            return None
+        self.send_response(200)
+        self.send_header("Content-type", ctype)
+        fs = os.fstat(f.fileno())
+        self.send_header("Content-Length", str(fs[6]))
+        self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
+
+        if (path.endswith("kif") or path.endswith("kif.txt")):
+            self.send_header("Pragma", "no-cache")
+
+        self.end_headers()
+        return f
+
+
+SimpleHTTPServer.test(HookHTTPRequestHandler)
